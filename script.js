6378fda1fa24e300b1d7f8adb7833986916b532f
rdnsa <rdnsa123@gmail.com>
2025-03-28 17:38:16 +0700

    code frontend sederhana

---
+++ script.js
@@ -0,0 +1,8 @@
+function ubahTeks() {
+    let judul = document.getElementById("judul");
+    if (judul.innerHTML === "Halo, Rizz! 🚀") {
+        judul.innerHTML = "Selamat Belajar Frontend! 🎉";
+    } else {
+        judul.innerHTML = "Halo, Rizz! 🚀";
+    }
+}
